ddb096a059035a40e862ace5d10a76749440841c
jonghyeonkim <whdgus8219@naver.com>
2020-12-31 01:57:53 +0900

    Init repository and Add queryDSL example

---
+++ src/main/java/com/study/querydsl/controller/HelloController.java
@@ -0,0 +1,4 @@
+package com.study.querydsl.controller;
+
+public class HelloController {
+}
---
+++ src/test/java/com/study/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,41 @@
+package com.study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.study.querydsl.entiry.Hello;
+import com.study.querydsl.entiry.QHello;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+@Transactional
+@Commit
+class QuerydslApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertEquals(result, hello);
+        assertEquals(result.getId(), hello.getId());
+
+    }
+
+}
